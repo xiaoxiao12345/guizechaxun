67ae8752f9e950f793e35aaaed20132af9661e9c
xiaoxiao <1136283883@qq.com>
2018-03-31 11:29:59 +0800

    修改后的编码风格，可以找到要的页面，而且应该用import pdb这个工具，当进入函数直接挺了，这很直观，不是打印出来的。

--- selectpage.py
+++ selectpage.py
@@ -8,19 +8,19 @@ from pymongo import MongoClient
 def get_ruleid(name):
     client = MongoClient('192.168.0.102',27017)
     dev_db = client.devplatform
-    gz_db = client.gz_opinion
     dev_collection = dev_db.rule.find({"alias":name})
     for dev_item in dev_collection:
         print(dev_item)
         if isinstance(dev_item, dict):
             rule_id = dev_item.get("_id")
-            print("rule_id", str(rule_id))
+            return rule_id
         else:
             print("no rule")
 
+def get_page_equal_ruleid(rule_id):
+    client = MongoClient('192.168.0.102', 27017)
+    gz_db = client.gz_opinion
     gz_collection = gz_db.page_setup.find({},{"relations":1})
-    print(gz_collection.count())
-
     for item in gz_collection:
         if not item.has_key("relations"):
             continue
@@ -33,25 +33,12 @@ def get_ruleid(name):
 
             for tag_name, tag_value in value['value']['module']['binding'].items():
                 relation_ruleid = tag_value['datasource']['id']
-                print("relation_ruleid", relation_ruleid.encode('utf-8'))
                 if relation_ruleid.encode('utf-8') == str(rule_id):
+                    import pdb; pdb.set_trace()
                     print('found')
 
-            '''    
-            try:
-                if isinstance(uuid.UUID(key), uuid.UUID):
-                    if item["relations"]["MDict"][key]["value"]["module"]["binding"]:
-                        for binding_key ,value in item['relations']['MDict'][key]['value']['module']['binding'].items():
-                            relation_ruleid = item["relations"]["MDict"][key]["value"]["module"]["binding"][binding_key]["datasource"]["id"]
-                            print(relation_ruleid+"/n")
-                            if relation_ruleid == rule_id:
-                                print("get relation_ruleid == rule_id")
-                                print("relation_ruleid:"+relation_ruleid)
-                                print("\n")
-            except:
-                pass
 
-            '''
 
 if __name__ == "__main__":
-    get_ruleid("涉华外媒group查询")
+    rule_id= get_ruleid("涉华外媒group查询")
+    get_page_equal_ruleid(rule_id)
