b03216aa7eb78d8586cf3f3580bb2274e42fecd0
xiaoxiao <1136283883@qq.com>
2018-04-02 15:50:13 +0800

    最终版，兼容了collection ，写法不同db[system],如果写成db.system就不行，自己看着咯

--- selectpage.py
+++ selectpage.py
@@ -7,9 +7,9 @@ from pymongo import MongoClient
 from docx import Document
 from docx.shared  import Pt
 
-def get_rule_name(url):
+def get_rule_name(url, system):
     client = MongoClient('192.168.0.102', 27017)
-    gz_db = client.gz_opinion
+    gz_db = client[system]
     collection = gz_db.autodesign2.find({},{"objs":1,"_id":0})
     rule_name = []
     for item in collection:
@@ -33,7 +33,6 @@ def get_rule_name(url):
 
                         if data_item['data']['url'] == url:
                             key = key.encode('gbk')
-                            print(item['objs'][key]['alias'])
                             rule_name.append(item['objs'][key]['alias'])
 
     return rule_name
@@ -49,10 +48,10 @@ def get_ruleid(name):
         else:
             print("no rule")
 
-def get_page_equal_ruleid(rule_id):
+def get_page_equal_ruleid(rule_id, system):
     page = []
     client = MongoClient('192.168.0.102', 27017)
-    gz_db = client.gz_opinion
+    gz_db = client[system]
     gz_collection = gz_db.page_setup.find({},{"relations":1})
     for item in gz_collection:
         if not item.has_key("relations"):
@@ -87,14 +86,15 @@ class gen_doc():
     def __init__(self, **kwargs):
         self.name = kwargs.pop('name', None)
         self.url = kwargs.pop('url', None)
+        self.system = kwargs.pop('system', None)
 
     def run(self):
-        if self.url :
+        if self.url and self.system:
             if isinstance(self.name, list):
                 self.document.add_heading(text=u'与' + self.url + u'相关的规则与页面', level=1)
                 for name_item in self.name:
                     rule_id = get_ruleid(name_item)
-                    page = get_page_equal_ruleid(rule_id)
+                    page = get_page_equal_ruleid(rule_id, self.system)
                     if isinstance(page,list):
                         self.document.add_heading(text=u'用了' + name_item + u'规则的页面', level=2)
                         for item in page:
@@ -116,13 +116,13 @@ class gen_doc():
 def main():
     url ="http://61.164.49.130:13001/statisticsTask/api/result/list"
     name =""
-    print(type(name))
-    if url:
-        rule_name = get_rule_name(url)
-        get_doc = gen_doc(url=url,name=rule_name)
+    system= "gz_opinion"
+    if url and system:
+        rule_name = get_rule_name(url, system)
+        get_doc = gen_doc(url=url,name=rule_name,system=system)
         get_doc.run()
-    if name:
-        get_doc = gen_doc(name=name)
+    if name and system:
+        get_doc = gen_doc(name=name, system=system)
         get_doc.run()
 
 
